401507f3d31325496f0363f6037bf6ebf2309a6d
Joshua Blank <jmnblnk@yahoo.com>
2020-06-24 15:46:56 -0700

    added end page with styling and functionality

--- assets/end.js
+++ assets/end.js
@@ -1,9 +1,27 @@
 const username = document.getElementById("el");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
+
+const newScore = localStorage.getItem("newScore");
+const finalScore = document.getElementById("finalScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const maxHighScores = 5;
+
+finalScore.innerText = newScore;
 username.addEventListener("keyup", () => {
 saveScoreBtn.disabled = !username.Value;
 });
 
 saveHighScore = e => {
     e.preventDefault();
-}
+    const score = {
+        score: newScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+};
--- assets/script.js
+++ assets/script.js
@@ -42,8 +42,8 @@ var questions = [
     option2: "a Javascript library.",
     option3: "an ajax call.",
     option4: "a mathematical function",
-    answer: option2,
-  }
+    answer: 2
+  },
 
   {
     question: "API stands for :",
@@ -51,8 +51,8 @@ var questions = [
     option2: "Application Printer Interface.",
     option3: "Again, Possible Insanity.",
     option4: "Application Programming Interface.",
-    answer: option4,
-  }
+    answer: 4
+  },
 
   {
     question: "The difference between var and let is :",
@@ -60,71 +60,71 @@ var questions = [
     option2: "none; They are interchangeable.",
     option3: "scope. Let can only be accessed within the block of code where it is declared while var is global.",
     option4: "let is old and rarely used anymore.",
-    answer: option3,
-  }
+    answer: 3,
+  },
 
-{
+  {
     question: "A boolean variable is :",
     option1: "either true or false.",
     option2: "a variable declaration exclusive to IOS.",
     option3: "a variable declaration exclusive to Windows OS.",
     option4: "more of a conditional statement than an actual variable.",
-    answer: option1,
-}
+    answer: 1
+  },
 
-{
+  {
     question: "What is an event in Javascript?",
     option1: "The annual JS convention in Vegas.",
     option2: "When APIs are used to pull information from a 3rd party.",
     option3: "Another way of describing a website being deployed.",
     option4: "When Javascript interacts with HTML.",
-    answer: option4,
-}
+    answer: 4
+  },
 
-{
+  {
     question: "Where is the Javascript placed inside an HTML file?",
     option1: "Within the <script> tags.",
     option2: "After id declarations but never class declarations.",
     option3: "After class declarations but never id declarations.",
     option4: "After the closing HTML tag.",
-    answer: option1,
-}
+    answer: 1
+  },
 
-{
-    question: ""
-    option1:
-    option2:
-    option3:
-    option4:
-    answer:
-}
+  {
+    question: "Where is local storage actually stored?",
+    option1: "In the cloud.",
+    option2: "On a flash drive provided by the user.",
+    option3: "Within the local storage of the user's device",
+    option4: "It is a misleading term. You are actually supposed to write these things down.",
+    answer: 3
+  },
 
-{
-    question: 
-    option1:
-    option2:
-    option3:
-    option4:
-    answer:
-}
+  {
+    question: 'Using Jquery, which of the following is the same as: document.getElementById(/"testScore/");?',
+    option1: '$(/"element.id/").testScore',
+    option2: '$(/"document/").testScore',
+    option3: '$(/".testScore/")',
+    option4: '$(/"#testScore/")',
+    answer: 4
+  },
 
-{
-    question: 
-    option1:
-    option2:
-    option3:
-    option4:
-    answer:
-}
+  {
+    question: "What does HTML stand for?",
+    option1: "Hyper Markup Language Text.",
+    option2: "Hyper Text Markup Language.",
+    option3: "Hypertrophy Muscle Lengthening.",
+    option4: "Hypo Text Markup Language.",
+    answer: 2
+  },
 
-{
-    question: 
-    option1:
-    option2:
-    option3:
-    option4:
-    answer:
-}
+  {
+    question: "Javascript executes code :",
+    option1: "All at the same time.",
+    option2: "In reverse order.",
+    option3: "Sequentially.",
+    option4: "In the order dicated by HTML.",
+    answer: 3
+  },
 
 ];
 
@@ -140,12 +140,13 @@ quizStart = () => {
 
 newQuestion = () => {
   if (questionLog.length === 0 || questionNumber >= questionsMax) {
-    return window.location.assign("/finish.html");
+    localStorage.setItem("newScore", score);
+    return window.location.assign("./finish.html");
   }
   questionNumber++;
   questionCounterText.innerText = questionNumber + "/" + questionsMax;
   const questionIndex = Math.floor(Math.random() * questionLog.length);
-    liveQuestion = questionLog[questionIndex;]
+    liveQuestion = questionLog[questionIndex];
     question.innerText = liveQuestion.question;
   options.forEach(option =>{
     const number = option.dataset["number"];
