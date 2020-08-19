51035fc8d1e69d6dd9fae25588cafc8c4c053ca6
Joshua Blank <jmnblnk@yahoo.com>
2020-08-19 16:29:34 -0700

    update end.js

--- assets/end.js
+++ assets/end.js
@@ -1,27 +1,35 @@
-const username = document.getElementById("el");
+const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
-
-const newScore = localStorage.getItem("newScore");
 const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const highScores =JSON.parse(localStorage.getItem("highScores")) || [];
 const maxHighScores = 5;
 
-finalScore.innerText = newScore;
-username.addEventListener("keyup", () => {
-saveScoreBtn.disabled = !username.Value;
+console.log(highScores);
+
+finalScore.innerText = mostRecentScore;
+
+
+
+username.addEventListener('keyup', () =>{
+    saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = e => {
+saveHighScore = (e) => {
+    console.log("click the save button!");
     e.preventDefault();
+
     const score = {
-        score: newScore,
+        score: mostRecentScore,
         name: username.value
     };
     highScores.push(score);
-    highScores.sort( (a, b) => b.score - a.score);
+    highScores.sort( (a,b) => b.score - a.score);
     highScores.splice(5);
 
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("/");
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('../index.html');
+
+    console.log(score);
 };
