532b5efd05260379c9467bf485f63bb3aaa7fe13
Joshua Blank <jmnblnk@yahoo.com>
2020-08-28 11:14:24 -0700

    complete question list

--- assets/quiz.js
+++ assets/quiz.js
@@ -12,123 +12,123 @@ let availableQuestions = [];
 
 let questions = [
     {
-        question: "What is the correct sticking for a Paradiddle?",
-        choice1: "RLLL RLLL",
-        choice2: "RRRR LLLL",
-        choice3: "RRLL RRLL",
-        choice4: "RLRR LRLL",
-        answer: 4
-    },
-    {
-        question: "What instrument family does the piano belong to?",
-        choice1: "Woodwinds",
-        choice2: "Strings",
-        choice3: "Percussion",
-        choice4: "Brass",
-        answer: 3
-    },
-    {
-        question: "Which term is not a common slang for Drum Set?",
-        choice1: "Trap Kit",
-        choice2: "Tubs",
-        choice3: "Skins",
-        choice4: "Barrels",
-        answer: 4
-    },
-    {
-        question: "Which is not a common technique for how to hold drumsticks?",
-        choice1: "French Grip",
-        choice2: "MatchGrip",
-        choice3: "Parallel Grip",
-        choice4: "Match Grip",
-        answer: 3
-    },
-    {
-        question: "How many times do you strike the drum when performing a 9-Stroke Roll?",
-        choice1: "18",
-        choice2: "9",
-        choice3: "36",
-        choice4: "27",
+        question: "What goes inside square brackets []?",
+        choice1: "Link to another page",
+        choice2: "Array",
+        choice3: "Object",
+        choice4: "Variable Declaration",
         answer: 2
     },
     {
-        question: "Which Drummer has toured with Sting, NIN, Weezer and DEVO?",
-        choice1: "Josh Freese",
-        choice2: "Kenny Aranoff",
-        choice3: "Steve Gadd",
-        choice4: "Ringo Starr",
-        answer: 1
-    },
-    {
-        question: "What is another name for a Kettle Drum?",
-        choice1: "Timbale",
-        choice2: "Timpani",
-        choice3: "Side Drum",
-        choice4: "Snare Drum",
-        answer: 2
-    },
-    {
-        question: "Which of these is a common Drum Rudiment?",
-        choice1: "Skip To My Lou",
-        choice2: "Hot Potato",
-        choice3: "Flamacue",
-        choice4: "Chicka Chicka",
+        question: "To insert a JavaScript into an HTML page, which tag is used?",
+        choice1: "<script='java'>",
+        choice2: "<javascript>",
+        choice3: "<script>",
+        choice4: "<js>",
         answer: 3
     },
     {
-        question: "How many drums make a 5 piece drum set?",
-        choice1: "5",
-        choice2: "4",
-        choice3: "6",
-        choice4: "10",
-        answer: 1
-    },
-    {
-        question: "Which of these is not a playable time signature?",
-        choice1: "13/8",
-        choice2: "4/4",
-        choice3: "6/8",
-        choice4: "7/7",
-        answer: 4
-    },
-    {
-        question: "A Flam is a single hit with what right before it?",
-        choice1: "A Buzz",
-        choice2: "A Diddle",
-        choice3: "A Grace Note",
-        choice4: "An Accent",
-        answer: 3
-    },
-    {
-        question: "Which style of drumming originated in New Orleans?",
-        choice1: "Second Line",
-        choice2: "Rudimental",
-        choice3: "Orchestral",
-        choice4: "Rock",
-        answer: 1
-    },
-    {
-        question: "The back beat is played on what beats?",
-        choice1: "2 and 3",
-        choice2: "1 and 3",
-        choice3: "3 and 4",
-        choice4: "2 and 4",
-        answer: 4
-    },
-    {
-        question: "When a musician says they are going to woodshed, they mean they plan to...",
-        choice1: "Put away the Lawnmower",
-        choice2: "Practice",
-        choice3: "Get a dayjob",
-        choice4: "Play outside",
+        question: "Which of the following is a server-side Java Script object?",
+        choice1: "Function",
+        choice2: "File",
+        choice3: "FileUpload",
+        choice4: "Date",
         answer: 2
     },
     {
-        question: "What makes a good Drummer?",
-        choice1: "How fast they can play",
-        choice2: "How many licks they know",
-        choice3: "Good time keeping, rhythm, dynamics, instincts and general musicality",
-        choice4: "looks, hair and attitude",
+        question: "Which of the following means 'or'?",
+        choice1: "&&",
+        choice2: "=>",
+        choice3: "||",
+        choice4: "<=",
+        answer: 3
+    },
+    {
+        question: " How does Java Script store dates in objects of Date type?",
+        choice1: "The number of days since January 1st, 1900",
+        choice2: "The number of seconds since January 1st, 1970",
+        choice3: "The number of milliseconds since January 1st, 1970",
+        choice4: "The number of picoseconds since January 1st, 1970",
+        answer: 3
+    },
+    {
+        question: "JavaScript ignores:",
+        choice1: "bad coders",
+        choice2: "special characters",
+        choice3: "white space",
+        choice4: "yellow space",
+        answer: 3
+    },
+    {
+        question: "How do we write 'Hello World' in an alert box?",
+        choice1: "alertBox('Hello World')",
+        choice2: "box('Hello World')",
+        choice3: "alert('Hello World')",
+        choice4: "('Hello World').alertBox",
+        answer: 3
+    },
+    {
+        question: "Which is the correct way to write a JavaScript array?",
+        choice1: "var txt = ['kim', 'jim']",
+        choice2: "var txt = new Array:1=(' arr ')2=('kim')3=('jim')",
+        choice3: "var txt = new Array('arr ','kim','jim')",
+        choice4: "var txt = new Array= ' arr ','kim','jim'",
+        answer: 1
+    },
+    {
+        question: "Which of the following can you not declare as a variable?",
+        choice1: "jumpButton",
+        choice2: "document",
+        choice3: "facePalm",
+        choice4: "tenTimes",
+        answer: 2
+    },
+    {
+        question: "Which language gives a website functionality?",
+        choice1: "HTML",
+        choice2: "CSS",
+        choice3: "Github",
+        choice4: "JavaScript",
+        answer: 4
+    },
+    {
+        question: "Which of the following is an IF statement in JavaScript?",
+        choice1: "if i = 5",
+        choice2: "if i = 5 then",
+        choice3: "if (i == 5)",
+        choice4: "All of these",
+        answer: 3
+    },
+    {
+        question: "Which of the following is a comment in JavaScript?",
+        choice1: "<-This one->",
+        choice2: "//This one",
+        choice3: "/*This one*/",
+        choice4: "%This one%",
+        answer: 2
+    },
+    {
+        question: "What does css stand for?",
+        choice1: "cache static styles",
+        choice2: "cascading sheets styled",
+        choice3: "crazy super styling",
+        choice4: "cascading style sheets",
+        answer: 4
+    },
+    {
+        question: "Which operator is used to assign a value to a variable in JavaScript?",
+        choice1: "$",
+        choice2: "=",
+        choice3: "()",
+        choice4: "#",
+        answer: 2
+    },
+    {
+        question: "What is the difference between HTTP and HTTPS?",
+        choice1: "There is no difference.",
+        choice2: "One is plural, the other singular.",
+        choice3: "The 'S' stands for Secure.",
+        choice4: "HTTPS is outdated and rarely used.",
         answer: 3
     },
 
@@ -140,7 +140,7 @@ const correctBonus = 10;
 const maxQuestions = 10;
 const incorrectPenalty = -10;
 
-startGame = () => {
+startQuiz = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions]
@@ -218,4 +218,4 @@ decreaseTimer = num => {
     timer.innerText = timer;
 }
 
-startGame(); 
+startQuiz(); 
